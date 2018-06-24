f14e3b7ec67c45ea1c8dd337a008011e9ad30fb3
Simon J.K. Pedersen <mail@sjkp.dk>
2018-06-24 20:58:52 +0200

    Add project files.

---
+++ Delegate.MLTraining/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Delegate.MLTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
